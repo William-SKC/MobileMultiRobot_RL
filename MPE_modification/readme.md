9068aa034f8e0a22bea5081979c3ec5448c3be5f
Shengkang Chen <shengkangchen@Shengkangs-MacBook-Pro.local>
2024-11-26 16:54:00 -0800

    added mpe modifications, need to fix memeory leak

---
+++ MPE_modification/readme.md
@@ -0,0 +1,2 @@
+
+Modification loc: /opt/anaconda3/envs/pettingzoo_m4/lib/python3.11/site-packages/pettingzoo/mpe/_mpe_utils/core.py
---
+++ MPE_modification/simple_nav_v1.py
@@ -0,0 +1,3 @@
+from pettingzoo.mpe.simple_nav.simple_nav import env, parallel_env, raw_env
+
+__all__ = ["env", "parallel_env", "raw_env"]
